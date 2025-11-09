c22c15d9d3bb412450ad72ee1f554b9567aafc55
taran1s <taran1sdev@gmail.com>
2025-11-09 17:15:23 +0000

    Listen on tcp and print incoming data

--- main.go
+++ main.go
@@ -3,25 +3,62 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
-	"os"
+	"net"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str
+				str = ""
+			}
+
+			str += string(data)
+		}
+
+		if len(str) != 0 {
+			out <- str
+		}
+	}()
+
+	return out
+}
+
 func main() {
-	f, err := os.Open("messages.txt")
+	listener, err := net.Listen("tcp", ":8888")
 	if err != nil {
 		log.Fatal("error", "error", err)
 	}
 
-	str := ""
 	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
+		conn, err := listener.Accept()
 		if err != nil {
-			break
+			log.Fatal("error", "error", err)
 		}
 
 		data = data[:n]
+
+		for line := range getLinesChannel(conn) {
+			fmt.Printf("read: %s\n", line)
+		}
 
 		if i := bytes.IndexByte(data, '\n'); i != -1 {
 			str += string(data[:i])
